888ec94f62c23d9d7ce7226550baad00347c1cd6
rvd2pwipwip <herve_desrosiers@hotmail.com>
2019-01-05 18:58:00 -0500

    reverse a string recursively

---
+++ Basic Algorithm Scripting/Reverse a String/js/reverseString.js
@@ -0,0 +1,25 @@
+//function reverseString(str) {
+//  let revStr = '';
+//  let strArr = str.split('');
+//  for (let i = strArr.length; i > 0; i--) {
+//    revStr += strArr[i - 1];
+//  }
+//  return revStr;
+//}
+
+//the recursive way
+//function reverseString(word) {
+//	if (word.length === 0) return;
+//	if (word.length === 1) return word;
+//	if (word.length > 0) {
+//		return word[word.length - 1] += reverseString(word.substring(0, word.length - 1))
+//	}
+//}
+
+function reverseString(str) {
+	return str.length === 0 ? "" : 
+		str.length === 1 ? str : 
+		str[str.length - 1] += reverseString(str.substring(0, str.length - 1))
+}
+
+console.log(reverseString("hello"));
